b9f1278e3ac03c3a904c90dea0c5284485b1b657
Manisha Manisha <Manisha4577@gmail.com>
2023-05-04 22:11:54 +0530

    added code to read xls file

--- src/main/java/boauiflip/testdatareader/DataReader.java
+++ src/main/java/boauiflip/testdatareader/DataReader.java
@@ -1,5 +1,102 @@
 package boauiflip.testdatareader;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
 public class DataReader {
+	
+	
+	
+	public Map<Integer, List<Object>> readxlsFile(String xlsFileName,String subsheet)
+	{
+		Map<Integer, List<Object>> map=new LinkedHashMap<Integer, List<Object>>();
+		try {
+			
+			String path=System.getProperty("user.dir")+"\\testdata\\"+xlsFileName;
+			FileInputStream file=new FileInputStream(path);
+			
+			Workbook book=WorkbookFactory.create(file);
+			Sheet sheet=book.getSheet(subsheet);
+			
+			Iterator<Row> itr=sheet.iterator();
+			
+			while(itr.hasNext())
+			{
+				List<Object> rowlist=new ArrayList<Object>();
+				Row row=itr.next();
+				
+				Iterator<Cell> cellitr=row.cellIterator();
+				int i=0;
+				while(cellitr.hasNext())
+				{
+					
+					
+					Cell cellvalue=cellitr.next();
+					
+					
+					switch (cellvalue.getCellType()) {
+					case STRING:
+						rowlist.add(cellvalue.getStringCellValue());
+						break;
+						
+					case BOOLEAN:
+						
+						rowlist.add(cellvalue.getBooleanCellValue());
+						
+						break;
+						
+					case NUMERIC:
+						rowlist.add(cellvalue.getNumericCellValue());
+						break;
+						
+					case FORMULA:
+						
+						rowlist.add(cellvalue.getCellFormula());
+						
+						break;
+						
+						
+					case BLANK:
+						
+						rowlist.add("Blank");
+						break;
+
+					default:
+						System.out.println("None of the above");
+						break;
+					}
+					
+				}
+				
+				map.put(i, rowlist);
+				i++;
+				
+				
+			}
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return map;
+		
+		
+		
+		
+		
+	}
+	
+	
+	
 
 }
